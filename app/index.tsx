c9f60c3443b8276544303f650ceb6baccfc0b609
DanDuran09 <daniel.duran.torres09@gmail.com>
2025-03-31 00:05:07 -0600

    add : new style for tab index

--- app/index.tsx
+++ app/index.tsx
@@ -4,13 +4,29 @@ import { Colors } from "react-native-ui-lib";
 import Cart from "./screens/Cart";
 import Settings from "./screens/Settings";
 import History from "./screens/History";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function index() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="scanner">
+    <Tab.Navigator initialRouteName="scanner" screenOptions={{
+      tabBarStyle: {
+        position:'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: Colors.blue40,
+        borderTopLeftRadius: 20,
+        height:'7%',
+        paddingHorizontal:5,
+        borderTopRightRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: -2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+      },
+    }}    >
       <Tab.Screen
         name="scanner"
         component={Cart}
@@ -18,23 +34,31 @@ export default function index() {
           headerShown: false,
           title: "CARRITO",
           tabBarIcon: ({color})=> <FontAwesome  name="shopping-cart" color={color} size={30} />,
-          tabBarActiveTintColor: Colors.blue40,
+          tabBarActiveTintColor: Colors.white,
+          tabBarInactiveTintColor:Colors.grey10
         }}
       />
       <Tab.Screen
         name="Historial"
         component={History}
-        options={{ headerShown: false, title: "HISTORIAL" }}
+        options={{ headerShown: false, title: "HISTORIAL",
+        tabBarIcon: ({color})=> <FontAwesome  name="history" color={color} size={30} />,
+        tabBarActiveTintColor: Colors.white,
+        tabBarInactiveTintColor:Colors.grey10 }}
       />
       <Tab.Screen
         name="Inventario"
         component={MyStack}
-        options={{ headerShown: false, title: "INVENTARIO" }}
+        options={{ headerShown: false, title: "INVENTARIO" ,tabBarIcon: ({color})=> <FontAwesome  name="list" color={color} size={30} />,
+        tabBarActiveTintColor: Colors.white,
+        tabBarInactiveTintColor:Colors.grey10}} 
       />
       <Tab.Screen
         name="Perfil"
         component={Settings}
-        options={{ headerShown: false, title: "CONFIGURACIÓN" }}
+        options={{ headerShown: false, title: "CONFIGURACIÓN" ,tabBarIcon: ({color})=> <Ionicons  name="settings" color={color} size={30} />,
+        tabBarActiveTintColor: Colors.white,
+        tabBarInactiveTintColor:Colors.grey10}}
       />
     </Tab.Navigator>
   );
